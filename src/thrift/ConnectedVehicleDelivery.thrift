0b599b511b3db581d9ab068c6eabcd668bb11eef
Stefan Roth <dr.stefan.roth@googlemail.com>
2013-06-11 09:58:01 +0200

    Added delivery status to thrift file

--- src/thrift/ConnectedVehicleDelivery.thrift
+++ src/thrift/ConnectedVehicleDelivery.thrift
@@ -1,9 +1,21 @@
 namespace csharp Zuehlke.ConnectedVehicles
 
+typedef i64 Timestamp
+
 enum VehicleType {
 	CAR,
 	VAN,
 	BIKE
+}
+
+enum DeliveryStatusType {
+	START,
+	DRIVING,
+	REFUELLING,
+	STOP,
+	UNLOADING,
+	REPAIRING,
+	OTHER
 }
 
 struct Vehicle {
@@ -11,6 +23,17 @@ struct Vehicle {
 	2: required VehicleType vehicleType,
 	3: optional string vehicleModel,
 	4: optional string driver
+}
+
+struct DeliveryStatus {
+	1: required Timestamp timestamp,
+	2: required DeliveryStatusType statusType,
+	3: optional string additionalStatusInfo
+}
+
+struct Delivery {
+	1: required Vehicle vehicle,
+	2: required list<DeliveryStatus> statusHistory 
 }
 
 
