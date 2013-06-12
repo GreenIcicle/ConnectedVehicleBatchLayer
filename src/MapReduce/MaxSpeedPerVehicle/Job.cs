ff1ea431b41c848e9b5c0af126d037b00813016d
Vitaliy Slepakov <v.slepakov@gmail.com>
2013-06-12 10:26:13 +0200

    Add MaxSpeedPerVehicle Job

---
+++ src/MapReduce/MaxSpeedPerVehicle/Job.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Hadoop.MapReduce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zuehlke.Camp2013.ConnectedVehicles.MapReduce.MaxSpeedPerVehicle
+{
+    public class Job : HadoopJob<Mapper, Reducer>
+    {
+
+        public override HadoopJobConfiguration Configure(ExecutorContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/MapReduce/MaxSpeedPerVehicle/Mapper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Hadoop.MapReduce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zuehlke.Camp2013.ConnectedVehicles.MapReduce.MaxSpeedPerVehicle
+{
+    public class Mapper : MapperBase
+    {
+        public override void Map(string inputLine, MapperContext context)
+        {
+            var delivery = SerializationHelper.Deserialize<Delivery>(inputLine);
+
+            //TODO
+        }
+    }
+}
